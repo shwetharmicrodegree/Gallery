54b6f8dbf9c11d23d718c6bc654fc735d2f4aebb
shwetharmicrodegree <shwethar.microdegree@gmail.com>
2025-08-01 15:47:26 +0530

    full code of image upload django

---
+++ venv/gallery_project/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('gallery.urls')), # Include your gallery app URLs
+]
+
+# Serve media files during development (not for production with S3)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
